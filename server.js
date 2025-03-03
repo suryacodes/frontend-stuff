ffa9ccbbf49a3a418966b94bc3d8536aed2101f1
suryacodes <suryanarayanan2309@gmail.com>
2025-03-03 06:48:10 +0530

    content security policy

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require("express");
-const helmet = require("helmet");
 const path = require("path");
 
 const app = express();
 const PORT = 4000;
 
+// Middleware to parse JSON request bodies
+app.use(express.json({ type: ["application/csp-report", "application/json"] }));
+
+// Set Content Security Policy (CSP) headers
 app.use((req, res, next) => {
   res.setHeader(
-    "Permissions-Policy",
-    'geolocation=(self "https://pp-demo-trusted-site.glitch.me"), camera=(), microphone=()'
+    "Content-Security-Policy",
+    "default-src 'self';" +
+      "frame-src https://pp-demo-trusted-site.glitch.me;" +
+      `script-src 'self' 'nonce-randomkey';` +
+      "report-uri /csp-violation-report-endpoint;"
   );
   next();
 });
 
 // Serve static files from the React build folder
 app.use(express.static(path.join(__dirname, "build")));
+
+// CSP violation report endpoint
+app.post("/csp-violation-report-endpoint", (req, res) => {
+  // Handle the CSP violation report here
+  console.log("CSP Violation Report:", req.body);
+  res.status(204).end(); // Respond with a 204 status (No Content)
+});
 
 // Catch-all route to serve React's index.html (for React Router)
 app.get("/*", (req, res) => {
